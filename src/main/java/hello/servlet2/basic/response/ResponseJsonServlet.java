53607dd311a51ae6db38d4abe04f3f69a2d37937
master <ted3131@diquest.com>
2024-03-19 09:22:53 +0900

    commit

---
+++ src/main/java/hello/servlet2/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet2.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet2.basic.request.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper;
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setHeader("content-type", "application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+        //{"username":"kim","age":20}
+        String result = objectMapper.writeValueAsString(data);
+        response.getWriter().write(result);
+    }
+}
